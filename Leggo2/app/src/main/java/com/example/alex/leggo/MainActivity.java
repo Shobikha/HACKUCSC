6f2a8fbbc429c18eed060353487a1855c9951762
Alex <alex@eduroam-169-233-248-48.ucsc.edu>
2017-01-21 04:01:25 -0800

    new ACtivity starts

--- Leggo2/app/src/main/java/com/example/alex/leggo/MainActivity.java
+++ Leggo2/app/src/main/java/com/example/alex/leggo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.alex.leggo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton button = (ImageButton) findViewById(R.id.event);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                StartEvent();
+            }
+        });
     }
 
     public void StartEvent() {
